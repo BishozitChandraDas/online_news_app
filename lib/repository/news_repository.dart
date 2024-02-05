f0524376c6bbb79f5a915bf955f07fae8f83def5
Bishozit <bishozitdas191@gmail.com>
2024-02-05 19:33:43 +0600

    Code optimize

--- lib/repository/news_repository.dart
+++ lib/repository/news_repository.dart
@@ -4,15 +4,12 @@ import '../models/categories_new_model.dart';
 import '../models/news_channel_headlines_model.dart';
 
 class NewsRepository {
-
-
   Future<CategoriesNewsModel> fetchNewsCategoires(String category) async {
     String newsUrl =
         'https://newsapi.org/v2/everything?q=$category&apiKey=8a5ec37e26f845dcb4c2b78463734448';
     final response = await http.get(Uri.parse(newsUrl));
     if (response.statusCode == 200) {
       final body = jsonDecode(response.body);
-
       return CategoriesNewsModel.fromJson(body);
     } else {
       throw Exception('Error');
